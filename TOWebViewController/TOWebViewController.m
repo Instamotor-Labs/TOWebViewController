200a00fc9909b515ec8a5f5d46683388e29f3962
Sy Bohy <sy@instamotor.com>
2015-03-03 13:46:41 -0800

    Update TOWebViewController.m

--- TOWebViewController/TOWebViewController.m
+++ TOWebViewController/TOWebViewController.m
@@ -473,11 +473,11 @@ static const float kAfterInteractiveMaxProgressValue    = 0.9f;
     if (self.navigationController) {
         if (IPAD == NO) { //iPhone
             if (self.beingPresentedModally == NO) { //being pushed onto a pre-existing stack, so
-                [self.navigationController setToolbarHidden:self.navigationButtonsHidden animated:animated];
+                [self.navigationController setToolbarHidden:YES animated:animated];
                 [self.navigationController setNavigationBarHidden:NO animated:animated];
             }
             else { //Being presented modally, so control the
-                self.navigationController.toolbarHidden = self.navigationButtonsHidden;
+                self.navigationController.toolbarHidden = NO;
             }
         }
         else {
